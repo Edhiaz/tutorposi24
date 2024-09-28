972c681167f0cdc6885592abe6367aeb83a80f16
dhiaz <eunikoyul@gmail.com>
2024-09-28 15:04:54 +0700

    tambah test

---
+++ coba.py
@@ -0,0 +1,5 @@
+nama = input("siapa namanya mas/mbak? ")
+tahun_lahir = int(input("lahir tahun berpa? "))
+umur = 2024 - tahun_lahir
+
+print(f'{nama} umurnya {umur} tahun')
---
+++ tets.py
@@ -0,0 +1,1 @@
+print('inyong kencottt')
